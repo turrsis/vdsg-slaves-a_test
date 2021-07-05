501cea8316db3a3f44d84e5857b3fa879699d5ba
Turrsis <turrsis@gmail.com>
2021-07-05 15:22:12 +0300

    qqq

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,51 @@
 <template>
-  <v-app v-designer.true>
+  <v-app v-designer.true :full-height="true">
+    <v-app-bar
+        absolute
+        color="#ADD8E6"
+        dark
+        shrink-on-scroll
+        src="https://picsum.photos/1920/1080?random"
+        scroll-target="#scrolling-techniques-2"
+        
+      >
+        <template v-slot:img="{ props }">
+          <v-img
+            v-bind="props"
+            gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+          ></v-img>
+        </template>
+  
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  
+        <v-app-bar-title>Title</v-app-bar-title>
+  
+        <v-spacer></v-spacer>
+  
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+  
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+  
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-app-bar>
     <v-main>
-      <!--<HelloWorld/>-->
       <router-view/>
     </v-main>
+    <v-footer max-height="60px">
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Turrsis</strong>
+      </v-col>
+    </v-footer>
   </v-app>
-  <!--<div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>-->
 </template>
 
 <style lang="scss">
@@ -19,11 +55,11 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  padding: 50px;
+  //padding: 50px;
 }
 
 #nav {
-  padding: 30px;
+  //padding: 30px;
 
   a {
     font-weight: bold;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,75 @@
 <template>
-  <div class="home" v-designer.true>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-sheet v-designer.true>
+    <v-card>
+      <v-card-header>
+        <v-card-header-text>
+          <v-card-title>Общее</v-card-title>
+        </v-card-header-text>
+      </v-card-header>
+      <v-card-text>
+        В общем случае WebComponent невозможно детерминировать т.е. невозможно однозначно сопоставить исходный код с разметкой в браузере.
+        Причина в том, что исходник преобразовывается в функции, которые собственно и генерируют браузерную разметку.
+      </v-card-text>
+      <v-card-text>
+        Существует два подхода для описания компонента: декларативный в виде темплейта и императивный (jsx, React).
+      </v-card-text>
+      <v-card-text>
+        В случае с декларативным подходом детерминирование становиться возможным.
+        Разметка темплейта преобразовывается в виртуальное дерево по которому собственно и строится клиентская разметка.
+        Т.к. в процессе сборки приложения темплейт "компилируется", возможно подключиться к "компилятору" и обогатить исходный темплейт информацией, 
+        которая позволит однознаяно сопоставить исходный темплейт с клиентской разметкой и виртуальным деревом.
+      </v-card-text>
+      <v-card-text>
+        Таким образом, на основании браузерной разметки, становиться возможно редактировать исходный код темплейта.
+      </v-card-text>
+    </v-card>
+    <v-container fluid>
+      <v-row class="text-left" dense>
+        <v-col
+          cols="12"
+        >
+          О верстке
+        </v-col>
+        <v-col
+          cols="12"
+        >
+          Для WebBuilders одной из сложностей является CSS
+        </v-col>
+        <v-col
+          cols="12"
+        >
+          С появлением MaterialDesign эта проблема не актуальна
+        </v-col>
+      </v-row>
+      <v-row class="text-left" dense>
+        <v-col cols="6">
+          <v-img
+            :aspect-ratio="16/9"
+            src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+          ></v-img>
+        </v-col>
+        <v-col cols="6">
+          <v-img
+            :aspect-ratio="16/9"
+            src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"
+          ></v-img>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-sheet>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
 <style lang="less">
   .home {
-    padding: 20px;
+    
   }
 </style>
