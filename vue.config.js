bae8bcb57245acc8ccb1935cf80f93936153caa1
Turrsis <turrsis@gmail.com>
2021-07-10 11:26:58 +0300

    vuetify.js 3.0.0-alpha.8    &&     vue 3.1.4

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 const server = require('./src/plugins/designer/server.js')
-
+// 
+/*
+  ./node_modules/@vue/runtime-core/dist/runtime-core.esm-bundler.js?5c40
+        5935 - patchChildren
+        6008 - patchKeyedChildren
+        5238 - patch
+        5613 - processFragment
+        *5419 - patchKeyedChildren
+*/
 module.exports = {
   transpileDependencies: [
     'vuetify'
