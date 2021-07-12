dbe8ded4d416d6e343322f3de6d5b2d37dc0d618
Turrsis <turrsis@gmail.com>
2021-07-12 09:56:56 +0300

    qqqq

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@
         shrink-on-scroll
         src="https://picsum.photos/1920/1080?random"
         scroll-target="#scrolling-techniques-2"
-        
       >
         <template v-slot:img="{ props }">
           <v-img
@@ -15,23 +14,23 @@
             gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
           ></v-img>
         </template>
-  
+
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
-  
+
         <v-app-bar-title>Title</v-app-bar-title>
-  
+
         <v-spacer></v-spacer>
-  
+
         <v-btn icon>
-          <v-icon>mdi-magnify</v-icon>
+          <v-icon icon="mdi-magnify"></v-icon>
         </v-btn>
-  
         <v-btn icon>
-          <v-icon>mdi-heart</v-icon>
+          <v-icon icon="mdi-heart"></v-icon>
         </v-btn>
-  
         <v-btn icon>
-          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon icon="mdi-dots-vertical"></v-icon>
         </v-btn>
       </v-app-bar>
     <v-main>
