23585df26912a0a2cbb143ab5e7047e460e20f27
Turrsis <turrsis@gmail.com>
2021-09-21 19:37:03 +0300

    touch

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="about" v-designer.true>
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about" v-designer.true>
+        <h1>
+            This is an about page
+        </h1>
+    </div>
 </template>
--- src/views/HomeWrap.vue
+++ src/views/HomeWrap.vue
@@ -1,25 +1,26 @@
 <template>
-  <div class="HomeWrap" v-designer.true>
-    <h1>
-      xxxxxxxxxxxxxx
-    </h1>
-    <!--<v-card>
-      <h2>
+    <div class="HomeWrap" v-designer.true>
+        <h1>
+            xxxxxxxxxxxxxx
+        </h1>
+        <!-- <v-card>
+        <h2>
         <Home />
-      </h2>
-    </v-card>-->
-    <v-sheet>
-      <h2>
-        <Home />
-      </h2>
-    </v-sheet>
-  </div>
+        </h2>
+        </v-card> -->
+        <v-sheet>
+            <h2>
+                <Home/>
+            </h2>
+        </v-sheet>
+    </div>
 </template>
 <script>
-import Home from './Home.vue'
-
+import Home from './Home.vue';
 export default {
-  name: 'HomeWrap',
-  components: { Home },
-}
+    name: 'HomeWrap',
+    components: {
+        Home
+    }
+};
 </script>
