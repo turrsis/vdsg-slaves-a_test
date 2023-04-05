b8742022ddb897c10ebcc9b602e145b05404a029
turrsis <turrsis@gmail.com>
2023-04-05 14:14:07 +0300

    refactor:: MetadataConfig

--- .designer/metadata.templates.mjs
+++ .designer/metadata.templates.mjs
@@ -1,34 +1,32 @@
-export default {
-    groups: {
-        'grid': {
-            elements: ['v-col']
-        },
-    },
-    elements: {
-        'v-btn': {
-            label: 'button',
-            templates: {'v-btn-icon-3': {
-                label: 'icon #3',
-                template: {
-                    tag: { name: 'v-btn' },
-                    props: { icon:'' },
-                    children: [{
-                        tag: { name: 'v-icon' },
-                        props: { icon: 'mdi-numeric-3-circle-outline' },
-                    }]
-                }
-            }}
-        },
-        'v-col': {
-            label: 'column',
-            templates: {'v-col-custom-1': {
+export default [{
+    key: 'vuetify-2',
+    children: [{
+        key: 'buttons',
+        children: [{
+            key: 'v-btn-icon-3',
+            label: 'btn three icon circle',
+            src: { template: {
+                tag: { name: 'v-btn' },
+                props: { icon:'' },
+                children: [{
+                    tag: { name: 'v-icon' },
+                    props: { icon: 'mdi-numeric-3-circle-outline' },
+                }]
+            } }
+        }]
+    }, {
+        key: 'grids',
+        children: [{
+            key: 'columns',
+            children: [{
+                key: 'v-col-custom-1',
                 label: 'v-col custom',
-                template: {
+                src: { template: {
                     tag: { name: 'v-col' },
                     props: { cols: '12' },
                     children: ['col xxx']
-                }
-            }}
-        },
-    }
-}
+                } }
+            }]
+        }]
+    }]
+}]
